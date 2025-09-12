1d15f4aaa2932d7eeb38cba53ba1bfa49ac0073a
legatomkii <christianftw@gmail.com>
2025-09-12 11:20:24 -0700

    Moroes 0.0.2: Imported Gemini AI, created Google AI Studio account and created API key. Linked API key, set prompt and response token counts.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from moroes!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key=os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key=api_key)
+genai.types.GenerateContentResponseDict
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+)
+usage = response.usage_metadata
+print(response.text)
+print(f"Prompt tokens: {usage.prompt_token_count}")
+print(f"Response tokens: {usage.candidates_token_count}")
